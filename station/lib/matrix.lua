08de42352308604c88a2dea7b342db45fb9a0bd0
Simon <Shazuli@users.noreply.github.com>
2018-06-25 17:57:26 +0200

    Working Matrix Library
    With the help of David, the Matrix Library now works as expected. It uses a pre-defined table for the directions instead of calculating the sin cos values.

--- station/lib/matrix.lua
+++ station/lib/matrix.lua
@@ -1,43 +1,58 @@
 local API = {}
 
-function API.transpose(m)
-   local rotated = {}
-   for c, m_1_c in ipairs(m[1]) do
-      local col = {m_1_c}
-      for r = 2, #m do
-         col[r] = m[r][c]
-      end
-      table.insert(rotated, col)
-   end
-   return rotated
-end
-function API.rotate_CCW_90(m)
-   local rotated = {}
-   for c, m_1_c in ipairs(m[1]) do
-      local col = {m_1_c}
-      for r = 2, #m do
-         col[r] = m[r][c]
-      end
-      table.insert(rotated, 1, col)
-   end
-   return rotated
-end
+--[[
+local grid1 = {
+  {{"X"},{"X"},{"O"}},
+  {{"O"},{"O"},{"O"}},
+  {{"X"},{"X"},{"X"}}
+}
+local grid1 = {
+  {"X","X","O"},
+  {"O","O","O"},
+  {"X","O","X"}
+}
+--]]
 
-local function transpose()
+API.TCW = {
+  {0,-1},
+  {1,0}
+}
 
-end
-
-function API.rotate90(grid)
-  local t = {}
-  for i in pairs(grid) do
-    for d in pairs(grid) do
-
-    end
+API.TACW = {
+  {0,1},
+  {-1,0}
+}
+local function createEmptyMatrix(len)
+  local m = {}
+  for i=1,len do
+    table.insert(m,{})
   end
-  return t
+  return m
 end
 
-function API.rotate_CW_90(m)
-   return API.rotate_CCW_90(API.rotate_CCW_90(API.rotate_CCW_90(m)))
+function API.transpose(matrix,dir)
+  local m = createEmptyMatrix(#matrix)
+  if dir == 1 then
+    for i in pairs(matrix) do
+      for d in pairs(matrix[i]) do
+        local temp = matrix[i][d]
+        m[d][i] = temp
+      end
+    end
+  else
+
+  end
+  return m
 end
+
+function API.rotate(pos,dir)
+  local x,y,z = table.unpack(pos)
+  --print("Old Pos: ",x,y,z)
+  local newBlockPos = {dir[1][1]*x + dir[1][2]*z, y, dir[2][1]*x + dir[2][2]*z}
+  x,y,z = table.unpack(newBlockPos)
+
+  --print("New Pos: ",x,y,z)
+  return x,y,z
+end
+
 return API
