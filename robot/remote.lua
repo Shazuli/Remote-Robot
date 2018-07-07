8e591248adf7fd30704e068050a968550e4a70f6
Simon <Shazuli@users.noreply.github.com>
2018-07-07 11:25:52 +0200

    Better Compression and Scanning
    With the help of David, the compression and efficiency of the scanning and transmission of the robot have been greatly improved.

--- robot/remote.lua
+++ robot/remote.lua
@@ -4,10 +4,17 @@ local event = require("event")
 local ser = require("serialization")
 local tty = require("tty")
 local thread = require("thread")
+local sides = require("sides")
+
+local SCAN_TYPE_SLICE = 1
+local SCAN_TYPE_LAYER = 2
+local SCAN_TYPE_COMPLETE = 3
+
+local ORIENTATIONS = {sides.north, sides.east, sides.south, sides.east}
+local orientation = 1 -- Indicates which of the elements in the ORIENTATIONS list is the current
 
 local hardValues = {}
 local terrain = {}
-
 
 --c.tunnel.send()
 local function compress(val)
@@ -30,89 +37,144 @@ local function decompress(val)
   return val
 end
 
-local function scan()
-  local x = {}
-  print("Analyzing ..")
-  for i=-4,4 do
-    for d=-4,4 do
-      table.insert(x,c.geolyzer.scan(i,d))
+-- Convert a geolyzer scan result into a single 64-bit number
+local function blocksToBits(scanResult, blockCount)
+  local result = 0
+  local blockCount = blockCount or 64 -- Allow capping the number of blocks scanned
+  for i, v in pairs(scanResult) do
+    if (i > blockCount) then break end -- Break if it goes over the block count
+    result = result << 1 -- Shift the bits left
+    if v ~= 0 then -- A block was found
+      result = result | 1 -- set the right-most bit to 1
     end
   end
-  print("Done Analyzing.")
-  return x
+  return result
+end
+
+-- Scan the blocks in a slice when moving along the XZ plane.
+-- orientation: Orientation of the slice
+-- distance: Distance of the slice from the robot
+local function scanSlice(facing, distance)
+  local result = {}
+  if facing & 1 == 0 then
+    distance = -distance -- If the robot is facing North or West, negate the distance
+  end
+  for i = 0, 2 do -- Scan each slice vertically
+    local blocks = nil
+    if facing & 4 == 4 then -- The robot is oriented on the East/West axis
+      blocks = blocksToBits(c.geolyzer.scan(distance, -4, -3 + i*7, 1, 9, 7), 63)
+    else -- The robot is oriented on the North/South axis
+      blocks = blocksToBits(c.geolyzer.scan(-4, distance, -3 + i*7, 9, 1, 7), 63)
+    end
+    table.insert(result, blocks)
+  end
+  return result
+end
+
+-- Scan a flat layer. Used when the robot moves up or down
+-- distance: The distance of the layer from the robot
+local function scanLayer(distance)
+  return {
+    blocksToBits(c.geolyzer.scan(-4, -4, distance, 9, 7, 1)),
+    blocksToBits(c.geolyzer.scan(-4,  3, distance, 9, 2, 1), 18) -- Only 18 blocks were scanned here
+  }
+end
+
+-- Do a complete scan by scanning the slices.Does not need to
+-- take orientation into account
+local function scan()
+  local result = {}
+  for i=-4,4 do
+    local slice = scanSlice(sides.south, i)
+    table.insert(result, slice)
+  end
+  return result
+end
+
+-- The info that should be sent along with the block data
+-- scan orientation
+-- Scan type: layer, slice, complete scan
+-- 000 - 000
+-- First three bits: scan orientation, last three bits: scan type
+local function sendTerrain(facing, scanType, blockData)
+  local scanInfo = 0
+  if scanType ~= SCAN_TYPE_COMPLETE then
+    scanInfo = facing << 3
+  end
+  scanInfo = scanInfo | scanType
+  c.tunnel.send("scanData", scanInfo, ser.serialize(blockData)) -- I don't think deflating it will give any benefit in this case since it's just numbers.
+end
+
+local function move(direction)
+  result, reason = c.robot.move(direction)
+  if result then
+    -- Scan the new blocks...
+    -- Give the station the new blocks
+  end
+  return result, reason
+end
+
+local function turn(direction)
+  if direction == true then -- Colckwise rotation
+    orientation = orientation % 4 + 1 -- If orientation goes to 5, cycle down to 1
+  else -- Counterclockwise/Anticlockwise rotation
+    orientation = (orientation - 2) % 4 + 1 -- If orientation goes to 0, then cycle up to 4.
+  end
+  result, reason = c.robot.turn(direction)
+  -- Tell the station that it rotated
+  return result, reason
+end
+
+-- Used for when the robot moves backwards, it will flip the orientation and scan as if it was
+-- facing forwards
+local function flippedOrientation()
+  return (orientation + 1) % 4 + 1
 end
 
 thread.create(function()
-  os.sleep(5)
+  os.sleep(3)
   tty.clear()
   print("Initilizing ..")
   --os.execute("/bin/components.lua")
   --c.gpu.bind("125bb046-a2c9-4182-8459-785265229229")
 end):detach()
 
-
-
 local function receive(_,_,_,_,_,msg1,msg2,msg3,msg4)
   --computer.beep()
   terrain = {}
   if msg1 == "scan" then
-    print("Starting Sequence ..")
-    --[[for x in pairs(hardValues) do
-      for z in pairs(hardValues[x]) do
-        for y in pairs(hardValues[x][z]) do
-          c.gpu.set(1,6,x .. " " .. y .. " " .. z)
-          if hardValues[x][z][y] > 0 then
-            table.insert(terrain,{x,y,z})
-            --print(x .. " " .. y .. " " .. z)
-            break
-          end
-        end
-      end
-    end--]]
-    --local x,z = -4,-4
-    for x=-4,4 do
-      --x = x + 1
-      for z=-4,4 do
-        local tile = c.geolyzer.scan(x,z)
-        --z = z + 1
-        --os.sleep(2)
-        --print(tile)
-        for y=#tile,1,-1 do
-          --print(y)
-          if tile[y] ~= 0 then
-            table.insert(terrain,{x,y-33,z})
-            --print("Found: ",x,y,z)
-            --break
-          end
-          if y < 31 then
-            break
-          end
-        end
-      end
-    end
-    print("Calculation Complete.")
-    print(#terrain .. " entries.")
-    print("Sending Data ..")
-    c.tunnel.send("scanData")
-    os.sleep(1)
-    for i=1,#terrain,4 do
-      os.sleep(1/20)
-      c.tunnel.send(
-        compress(terrain[i]),
-        compress(terrain[i+1]),
-        compress(terrain[i+2]),
-        compress(terrain[i+3])
-      )
-    end
-    print("Data Sent.")
-    os.sleep(1)
-    c.tunnel.send("scanDataComplete")
+    computer.beep()
+    print("Scanning...")
+    local terrain = scan()
+    sendTerrain(nil, SCAN_TYPE_COMPLETE, terrain)
+    computer.beep()
+    print("Scan complete.")
+    -- print("Starting Sequence ..")
+    -- local _,err = pcall(function()
+    -- terrain = scan()end)
+    -- if err then print(err) end
+    -- print(#terrain .. " entries.")
+    -- c.tunnel.send("scanData")
+    -- os.sleep(1)
+    -- for i in pairs(terrain) do
+    --   c.tunnel.send(terrain[i])
+    --   print(terrain[i])
+    -- end
+    -- print("Data Sent.")
+    -- os.sleep(1)
+    -- c.tunnel.send("scanDataComplete")
   end
   if msg1 == "mov" then
-    local status,err = pcall(function() load(msg2)() end)
-    if err then
-      print(err)
+    local result, reason = nil,nil
+    result, reason = load("return " .. msg2)()
+    if not result then
+    --print(reason)
+      print("Blocked")
+      c.tunnel.send("blocked")
+      return
     end
+    print("Moved")
+    c.tunnel.send("moved")
   end
 end
 
