aa49a1a5d1c9c4cedb47882fcaac2eae230e3c7c
Simon <Shazuli@users.noreply.github.com>
2018-07-08 18:44:50 +0200

    Beginning Auto-scan function + Graphical Inventory
    More output/input functions and commands.

--- robot/remote.lua
+++ robot/remote.lua
@@ -8,13 +8,14 @@ local sides = require("sides")
 
 local SCAN_TYPE_SLICE = 1
 local SCAN_TYPE_LAYER = 2
-local SCAN_TYPE_COMPLETE = 3
+local SCAN_TYPE_COMPLETE = 4
 
-local ORIENTATIONS = {sides.north, sides.east, sides.south, sides.east}
-local orientation = 1 -- Indicates which of the elements in the ORIENTATIONS list is the current
+local ORIENTATIONS = {sides.north, sides.east, sides.south, sides.west}
+local orientation = 2 -- Indicates which of the elements in the ORIENTATIONS list is the current
 
 local hardValues = {}
 local terrain = {}
+local charge = 1
 
 --c.tunnel.send()
 local function compress(val)
@@ -36,6 +37,13 @@ local function decompress(val)
   val = ser.unserialize(val)
   return val
 end
+
+local function turnOff()
+  c.tunnel.send("info","Shutting Down ..")
+  computer.shutdown()
+end
+
+--event.listen("energy_low",turnOff)
 
 -- Convert a geolyzer scan result into a single 64-bit number
 local function blocksToBits(scanResult, blockCount)
@@ -92,36 +100,47 @@ local function scan()
 end
 
 -- The info that should be sent along with the block data
+-- forward/backwards
 -- scan orientation
 -- Scan type: layer, slice, complete scan
--- 000 - 000
+-- 00 - 000 - 000
 -- First three bits: scan orientation, last three bits: scan type
-local function sendTerrain(facing, scanType, blockData)
-  local scanInfo = 0
-  if scanType ~= SCAN_TYPE_COMPLETE then
-    scanInfo = facing << 3
+local function sendTerrain(direction, facing, scanType, blockData)
+  local scanInfo = scanType
+  if direction then
+    scanInfo = scanInfo | (direction << 6)
   end
-  scanInfo = scanInfo | scanType
+  if facing then
+    scanInfo = scanInfo | (facing << 3)
+  end
   c.tunnel.send("scanData", scanInfo, ser.serialize(blockData)) -- I don't think deflating it will give any benefit in this case since it's just numbers.
 end
 
 local function move(direction)
-  result, reason = c.robot.move(direction)
+  local result, reason = c.robot.move(direction)
   if result then
-    -- Scan the new blocks...
-    -- Give the station the new blocks
+    if direction & 2 == 2 then -- Forward/backwards indicated by 2nd bit
+      local facing = ORIENTATIONS[orientation]
+      if direction == sides.back then -- If backwards, flip the 1st bit
+        facing = facing ~ 1 -- This will flip the 1st bit from whatever it currently is. 1 -> 0, 0 -> 1
+      end
+      print(direction)
+      sendTerrain(direction, facing, SCAN_TYPE_SLICE, scanSlice(orientation, 4))
+    else -- Up/down
+
+    end
   end
   return result, reason
 end
 
 local function turn(direction)
-  if direction == true then -- Colckwise rotation
+  if direction == true then -- Clockwise rotation
     orientation = orientation % 4 + 1 -- If orientation goes to 5, cycle down to 1
   else -- Counterclockwise/Anticlockwise rotation
     orientation = (orientation - 2) % 4 + 1 -- If orientation goes to 0, then cycle up to 4.
   end
   result, reason = c.robot.turn(direction)
-  -- Tell the station that it rotated
+  print(orientation)
   return result, reason
 end
 
@@ -130,6 +149,9 @@ end
 local function flippedOrientation()
   return (orientation + 1) % 4 + 1
 end
+local function initilize()
+  --Some stuff.
+end
 
 thread.create(function()
   os.sleep(3)
@@ -137,6 +159,8 @@ thread.create(function()
   print("Initilizing ..")
   --os.execute("/bin/components.lua")
   --c.gpu.bind("125bb046-a2c9-4182-8459-785265229229")
+  initilize()
+  print("Initilizing Complete")
 end):detach()
 
 local function receive(_,_,_,_,_,msg1,msg2,msg3,msg4)
@@ -146,7 +170,7 @@ local function receive(_,_,_,_,_,msg1,msg2,msg3,msg4)
     computer.beep()
     print("Scanning...")
     local terrain = scan()
-    sendTerrain(nil, SCAN_TYPE_COMPLETE, terrain)
+    sendTerrain(nil, nil, SCAN_TYPE_COMPLETE, terrain)
     computer.beep()
     print("Scan complete.")
     -- print("Starting Sequence ..")
@@ -165,20 +189,49 @@ local function receive(_,_,_,_,_,msg1,msg2,msg3,msg4)
     -- c.tunnel.send("scanDataComplete")
   end
   if msg1 == "mov" then
-    local result, reason = nil,nil
-    result, reason = load("return " .. msg2)()
-    if not result then
-    --print(reason)
-      print("Blocked")
-      c.tunnel.send("blocked")
-      return
+    if msg2 & 4 == 4 then -- left/right movement, indicated by the 3rd bit of msg2. 10X, if X is 0, then turn right, if X is 1, then turn left.
+      turn(msg2 == sides.right)
+    else
+      move(msg2) --Whatever other movement, use here.
     end
-    print("Moved")
-    c.tunnel.send("moved")
+  end
+  if msg1 == "getInventory" then
+    local inventory = {}
+    for i=1,16 do
+      c.robot.select(i)
+      local stack = {c.inventory_controller.getStackInInternalSlot()}
+      --os.sleep(1)
+      --print(stack["name"])
+      if #stack > 0 then
+        --print(stack[1][1])
+        stack = {
+          "Slot "..i,
+          stack[1]["label"],
+          math.floor(stack[1]["size"]).."/"..math.floor(stack[1]["maxSize"]),
+          stack[1]["name"],
+          math.floor(stack[1]["maxDamage"]-stack[1]["damage"]).."/"..math.floor(stack[1]["maxDamage"])
+        }--,stack[1]["name"]
+      else
+        stack = {"Slot "..i,"Empty"}
+      end
+      stack = ser.serialize(stack)
+      table.insert(inventory,stack)
+    end
+    c.tunnel.send("inventory",ser.serialize(inventory))
   end
 end
 
 event.listen("modem_message",receive)
+
+local tCharge = thread.create(function()
+  while true do
+    charge = computer.energy()/computer.maxEnergy()
+    if charge < 0.03 then turnOff() end
+    c.tunnel.send("charge",charge)
+    --print(charge)
+    os.sleep(20)
+  end
+end)
 
 local function update()
   os.sleep(1/20)
